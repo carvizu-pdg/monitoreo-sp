44e5d2139ab72f171f263110b75b87a2cb29bb4c
Cassandra Arvizu <cassandra.arvizu@prodigia.com.mx>
2022-09-29 18:21:05 -0700

    Agregamos autenticación

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Let's do thisssss!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <p>
+            ¡Hey {user.username}, ya iniciaste sesión!
+          </p>
+          <button onClick={signOut}>Salir</button>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
